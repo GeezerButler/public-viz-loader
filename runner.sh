203f9354acf0a39771a8bbbfa13ca3765c892c87
Aiman Ashraf <arahant7@Aimans-MacBook-Air.local>
2023-08-09 18:32:14 -0700

    continuos runner

--- runner.sh
+++ runner.sh
@@ -1,4 +1,10 @@
+# wait a little before starting
+sleep 30
+
+# keep generating load, until container stops
 while true
 do
     docker run -v ./screenshots:/app/screenshots public-load
+    echo "docker exit code: "
+    echo $?
 done
