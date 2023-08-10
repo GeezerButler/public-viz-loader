99ba83979d687c7207519f3d2da2c7aa49629a14
Aiman Ashraf <arahant7@Aimans-MacBook-Air.local>
2023-08-09 23:52:56 -0700

    append logs

--- runner.sh
+++ runner.sh
@@ -1,10 +1,8 @@
 # wait a little before starting
-sleep 30
+sleep 10
 
 # keep generating load, until container stops
 while true
 do
-    docker run public-load
-    echo "docker exit code: "
-    echo $?
+    docker run -v /home/ubuntu/public-viz-loader/screenshots:/app/screenshots public-load >> /home/ubuntu/docker.log
 done
